1d2b24b6fc45c12d4f272897d6d0fa4cc2de0fd7
Geoff Cant <nem@lisp.geek.nz>
2009-02-26 14:29:52 +0100

    Better edoc make target.

--- support/include.mk
+++ support/include.mk
@@ -41,6 +41,6 @@ $(EBIN_DIR)/%.$(EMULATOR): %.erl
 ./%.$(EMULATOR): %.erl
 	$(ERLC) $(ERLC_FLAGS) -o . $<
 
-$(DOC_DIR)/%.html: %.erl
-	$(ERL) -noshell -run edoc file $< -run init stop
-	mv *.html $(DOC_DIR)
+$(DOC_DIR)/index.html: $(EBIN_FILES_NO_DOCS)
+	$(ERL) -noshell -pa $(EBIN_DIR_ABS) -run edoc_run application "'mochiweb'" \
+               '"../"' '[{def,{vsn,"1.0"}}]'
